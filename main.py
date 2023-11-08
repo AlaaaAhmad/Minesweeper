0d3ae38c1ed555447e2d93e940abf549c4efad9b
Alaa <alaaahmed2032002@gmail.com>
2023-11-08 17:06:11 +0200

    First commit

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from tkinter import *
+import setting
+import utilities
 
 root = Tk()
-root.geometry("1200x520")
-root.resizable(False,False)
+root.configure(bg="Black")
 root.title("Minesweeper Game")
-root.config(bg="Black")
-root.mainloop()
+root.geometry(str(setting.WIDTH) + 'x' + str(setting.HEIGHT))
+root.resizable(False, False)
 
+top_frame = Frame(root,
+                  bg="Black",
+                  height=utilities.calc_height(25),
+                  width=setting.WIDTH)
+
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(root,
+                   bg="Black",
+                   height=utilities.calc_height(75),
+                   width=utilities.calc_width(25))
+
+left_frame.place(x=0, y=utilities.calc_height(25))
+
+center_frame = Frame(root,
+                     bg = "Black",
+                     height=utilities.calc_height(75),
+                     width=utilities.calc_width(75))
+center_frame.place(x = utilities.calc_width(25), y = utilities.calc_height(25))
+root.mainloop()
---
+++ utilities.py
@@ -0,0 +1,10 @@
+import setting
+
+
+def calc_height(percentage):
+    return (setting.HEIGHT / 100) * percentage
+
+
+def calc_width(percentage):
+    return (setting.WIDTH / 100) * percentage
+
